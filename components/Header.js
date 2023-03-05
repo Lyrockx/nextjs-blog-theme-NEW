4e856a0510696a954c72bcd3c337c5d34c68d691
Lyrockx <126742923+Lyrockx@users.noreply.github.com>
2023-03-05 14:10:24 -0500

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -3,12 +3,13 @@ import Link from 'next/link';
 export default function Header({ name }) {
   return (
     <header className="pt-20 pb-12">
-      <div className="w-12 h-12 rounded-full block mx-auto mb-4 bg-gradient-conic from-gradient-3 to-gradient-4" />
-      <p className="text-2xl dark:text-white text-center">
-        <Link href="/">
-          <a>{name}</a>
-        </Link>
-      </p>
-    </header>
+    <img src="https://github.com/Lyrockx/Image/blob/main/IMG_9521%202.jpeg?raw=true" alt="image description" className="w-20 h-20 rounded-full block mx-auto mb-4" />
+    <p className="text-2xl dark:text-white text-center">
+      <Link href="/">
+        <a>{name}</a>
+      </Link>
+    </p>
+  </header>
+
   );
 }
